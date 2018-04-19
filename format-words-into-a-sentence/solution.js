05e7cfa86c41c88bad6c8ad0742c762c3e50e00f
Earl Jay Caoile <ecaoile@my.hpu.edu>
2018-04-19 00:29:51 -0700

    finished Format Words into a Setence with readme, solution, and whiteboard picture

--- format-words-into-a-sentence/solution.js
+++ format-words-into-a-sentence/solution.js
@@ -8,34 +8,18 @@ function formatWords(words) {
       i--;
     }
   }
-
-  console.log(words);
   let formattedList = words.join(', ');
-  console.log(formattedList);
   if (words.length === 2) {
     formattedList = formattedList.replace(',', ' and');
   }
   else {
     formattedList = formattedList.replace(/,([^,]*)$/, ' and$1');
   }
-  // for (let i = 0; i < words.length; i++) {
-  //   console.log(words[i]);
-  //   if (i === words.length - 1 && words[i] !== '') {
-  //     console.log('alpha');
-  //     formattedList += words[i];
-  //   }
-  //   if (i === words.length - 2 && words[i] !== '') {
-  //     console.log('bravo');
-  //     formattedList += words[i] + ' and ';
-  //   }
-  //   if (i < words.length - 2 && words[i] !== '') {
-  //     formattedList += words[i] + ', ';
-  //     console.log('crap');
-  //   }
-  // }
   console.log(formattedList);
   return formattedList;
 }
 formatWords(['a']);
+formatWords(['a', 'b']);
 formatWords(['a', 'b', 'c', 'd']);
 formatWords(['one', '', 'three']);
+formatWords(['']);
